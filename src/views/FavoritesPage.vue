d472a0c3fc107fb69517f812899c0e2c7ceb9062
Phan Thị Thanh Nhàn <115072776+Thanhnhan1409@users.noreply.github.com>
2024-07-28 02:33:44 +0700

    fix: favorite page (#31)

--- src/views/FavoritesPage.vue
+++ src/views/FavoritesPage.vue
@@ -7,9 +7,9 @@
             <InputText placeholder="Bạn muốn đi đâu?" />
         </InputGroup>
         <div v-if="favoriteToursList.length" class="home-page-tour-cards">
-            <tour-card class="home-page-tour-cards-item" v-for="item in favoriteToursList" :key="item.id"
-                :heart-icon="activeFavoriteIcon[item.id] ? 'mdi:heart-circle-outline' : 'solar:heart-linear'"
-                :href="`detail/${item.id}`" :tour-item="item" @add-to-favorites="addToFavoriteTours(item.id)" />
+            <tour-card class="home-page-tour-cards-item" v-for="(item, index) in favoriteToursList" :key="item._id"
+                :heart-icon="activeFavoriteIcon[index] ? 'mdi:heart-circle-outline' : 'solar:heart-linear'"
+                :href="`detail/${item._id}`" :tour-item="item" @add-to-favorites="addToFavoriteTours(index)" />
         </div>
         <div v-else class="text-center m-auto px-6">
             <img src="//icons.veryicon.com/png/o/business/financial-category/no-data-6.png" class="opacity-50"
@@ -29,18 +29,21 @@ import { useRoute } from 'vue-router';
 import InputGroupAddon from 'primevue/inputgroupaddon';
 import InputGroup from 'primevue/inputgroup';
 import InputText from 'primevue/inputtext';
+import { getAllTimelines } from '@/services/timeline';
+import { Timeline } from './_id/DetailPage.vue';
 
 const route = useRoute()
 const favoriteTours = ref<any[]>([])
+const timelineList = ref<Timeline[]>([])
 const favoriteToursList = computed(() => {
-    return tours.filter(item => favoriteTours.value.includes(item.id))
+    return timelineList.value.filter((item, index) => favoriteTours.value.includes(index))
 })
 
 const activeId = ref<number>(1)
 const activeFavoriteIcon = ref<boolean[]>([])
 
 function filterActiveFavoriteTour() {
-    favoriteTours.value.forEach(item => activeFavoriteIcon.value[item] = true)
+    favoriteTours.value.forEach((item, index) => activeFavoriteIcon.value[index] = true)
     console.log(activeFavoriteIcon.value);
 
 }
@@ -56,6 +59,16 @@ function addToFavoriteTours(id: number) {
     }
     localStorage.setItem('favoriteTours', JSON.stringify(favoriteTours.value))
 }
+
+async function getAllTimelinesData() {
+    try {
+        const data = await getAllTimelines()
+        timelineList.value = data.data
+    } catch (error) {
+        console.error(error)
+    }
+}
+
 watch(() => route.fullPath, () => {
     const favoriteToursJson = localStorage.getItem("favoriteTours");
     if (favoriteToursJson !== null) {
@@ -70,7 +83,7 @@ watch(() => route.fullPath, () => {
     filterActiveFavoriteTour()
 })
 
-onBeforeMount(() => {
+onBeforeMount(async () => {
     const favoriteToursJson = localStorage.getItem("favoriteTours");
     if (favoriteToursJson !== null) {
         const tours = JSON.parse(favoriteToursJson);
@@ -81,7 +94,7 @@ onBeforeMount(() => {
         }
     }
     filterActiveFavoriteTour()
-
+    await getAllTimelinesData()
 })
 </script>
 
