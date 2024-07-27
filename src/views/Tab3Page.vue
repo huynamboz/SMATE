66887eaac6a01e4b2ca82e5ccf9f8774b471699c
Trịnh Huy Nam <38585889+huynamboz@users.noreply.github.com>
2024-07-27 14:03:19 +0700

    feat: load img from local (#19)

--- src/views/Tab3Page.vue
+++ src/views/Tab3Page.vue
@@ -12,7 +12,7 @@
           <Button class="w-[90%] rounded-full h-16" severity="secondary"  label="Đăng Ký" />
         </router-link>
       </div>
-      <img class="h-full w-full object-cover" src="https://images.unsplash.com/photo-1505142468610-359e7d316be0?q=80&w=1852&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="">
+      <img class="h-full w-full object-cover" src="@/assets/img/bg1.avif" alt="">
     </div>
   </ion-page>
 </template>
